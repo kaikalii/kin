2307889d22c10f53da615bf019bdc2fe9c9212ec
Kai Schmidt <kaikaliischmidt@gmail.com>
2021-04-27 13:23:36 -0700

    add commands

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod ast;
 mod parse;
 mod transpile;
 
+use clap::Clap;
+
 fn main() {
     use std::{path::PathBuf, process::*};
 
@@ -11,36 +13,58 @@ fn main() {
 
     color_backtrace::install();
 
-    let input = std::fs::read_to_string("test.noot").unwrap();
-    // Parse
-    match parse::parse(&input) {
-        Ok(items) => {
-            println!("Transpilation succeeded");
-            #[cfg(feature = "debug")]
-            println!("{:#?}", items);
-            // Transpile
-            let transpilation = transpile(items);
-            // Compile
-            transpilation.write().unwrap();
-            let status = Command::new(compiler_name())
-                .arg("build/main.c")
-                .arg("-o")
-                .arg(PathBuf::from("test").with_extension(EXE_EXT))
-                .arg("-O3")
-                .arg("-std=c99")
-                .spawn()
-                .unwrap()
-                .wait()
-                .unwrap();
-            if status.success() {
-                println!("Compilation succeeded");
+    let command = App::parse();
+
+    if command.builds() {
+        let input = std::fs::read_to_string("test.noot").unwrap();
+        // Parse
+        match parse::parse(&input) {
+            Ok(items) => {
+                println!("Transpilation succeeded");
+                #[cfg(feature = "debug")]
+                println!("{:#?}", items);
+                // Transpile
+                let transpilation = transpile(items);
+                // Compile
+                transpilation.write().unwrap();
+                let status = Command::new(compiler_name())
+                    .arg("build/main.c")
+                    .arg("-o")
+                    .arg(PathBuf::from("test").with_extension(EXE_EXT))
+                    .arg("-O3")
+                    .arg("-std=c99")
+                    .spawn()
+                    .unwrap()
+                    .wait()
+                    .unwrap();
+                if status.success() {
+                    println!("Compilation succeeded");
+                    if let App::Run = command {
+                        println!();
+                        Command::new("./test").spawn().unwrap().wait().unwrap();
+                    }
+                }
+            }
+            Err(errors) => {
+                for error in errors {
+                    println!("{}", error)
+                }
             }
         }
-        Err(errors) => {
-            for error in errors {
-                println!("{}", error)
-            }
-        }
+    }
+}
+
+#[derive(Clap)]
+enum App {
+    #[clap(alias = "r")]
+    Run,
+    #[clap(alias = "b")]
+    Build,
+}
+
+impl App {
+    fn builds(&self) -> bool {
+        matches!(self, App::Run | App::Build)
     }
 }
 
